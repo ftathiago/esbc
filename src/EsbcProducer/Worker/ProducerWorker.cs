009fd9c80a7795d98c942d2b160f6257275cd480
Francisco Thiago <ftathiago@gmail.com>
2020-07-26 16:21:03 -0300

    Refactor Worker to use scoped injection

--- src/EsbcProducer/Worker/ProducerWorker.cs
+++ src/EsbcProducer/Worker/ProducerWorker.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
@@ -9,28 +10,38 @@ namespace EsbcProducer.Worker
     public class ProducerWorker : BackgroundService
     {
         private readonly ILogger<ProducerWorker> _logger;
-        private readonly IProletarian _proletarian;
+        private readonly IServiceProvider _services;
 
-        public ProducerWorker(ILogger<ProducerWorker> logger, IProletarian proletarian)
+        public ProducerWorker(ILogger<ProducerWorker> logger, IServiceProvider services)
         {
-            _proletarian = proletarian;
             _logger = logger;
+            _services = services;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation($"Worker running at: {DateTimeOffset.Now}");
             while (!stoppingToken.IsCancellationRequested)
             {
                 try
                 {
-                    await _proletarian.DoWork(stoppingToken);
-                    await Task.Delay(5000, stoppingToken);
+                    using var scope = _services.CreateScope();
+                    var proletarian = scope.ServiceProvider.GetRequiredService<IMessageProducer>();
+                    _logger.LogInformation($"Worker running at: {DateTimeOffset.Now}");
+                    await DoWork(proletarian, stoppingToken);
                 }
                 catch (Exception exception)
                 {
-                    _logger.LogError(exception, "Error while sending message");
+                    _logger.LogError(exception, "A not handled error occurs. Reestarting all services");
                 }
+            }
+        }
+
+        private async Task DoWork(IMessageProducer proletarian, CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await proletarian.DoWork(stoppingToken);
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
