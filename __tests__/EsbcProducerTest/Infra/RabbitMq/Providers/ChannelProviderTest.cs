3d9db25cfc8160f0347e7b56d4dd1a367f3d7b92
Francisco Thiago <ftathiago@gmail.com>
2020-08-01 15:08:30 -0300

    Add new test and refactor tests

--- __tests__/EsbcProducerTest/Infra/RabbitMq/Providers/ChannelProviderTest.cs
+++ __tests__/EsbcProducerTest/Infra/RabbitMq/Providers/ChannelProviderTest.cs
@@ -1,36 +1,37 @@
-﻿using System;
-using EsbcProducer.Infra.RabbitMq.Providers;
+﻿using EsbcProducer.Infra.RabbitMq.Providers;
 using EsbcProducer.Infra.RabbitMq.Providers.Impl;
 using FluentAssertions;
 using Moq;
 using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace EsbcProducerTest.Infra.RabbitMq.Providers
 {
     public class ChannelProviderTest : IDisposable
     {
-        private readonly Mock<IConnectionProvider> _connectionProvider;
         private readonly Mock<IConnection> _connection;
-        private readonly IModel _channel;
+        private readonly Mock<IConnectionProvider> _connectionProvider;
+        private readonly Mock<IModel> _channel;
 
         public ChannelProviderTest()
         {
-            _connectionProvider = new Mock<IConnectionProvider>(MockBehavior.Strict);
-            _connectionProvider
-                .Setup(cp => cp.Dispose());
             _connection = new Mock<IConnection>(MockBehavior.Strict);
-            _connection
-                .Setup(c => c.Dispose());
-            var channel = new Mock<IModel>(MockBehavior.Strict);
-            channel
-                .Setup(c => c.Dispose());
-            _channel = channel.Object;
+
+            _connectionProvider = new Mock<IConnectionProvider>(MockBehavior.Strict);
+
+            _channel = new Mock<IModel>(MockBehavior.Strict);
+            _channel
+                .Setup(c => c.Dispose())
+                .Verifiable();
         }
 
         public void Dispose()
         {
-            Mock.VerifyAll();
+            _connectionProvider.Verify();
+            _connection.Verify();
+            _channel.Verify();
         }
 
         [Fact]
@@ -39,7 +40,7 @@ namespace EsbcProducerTest.Infra.RabbitMq.Providers
             // Given
             _connection
                 .Setup(c => c.CreateModel())
-                .Returns(_channel)
+                .Returns(_channel.Object)
                 .Verifiable();
             _connectionProvider
                 .Setup(cp => cp.GetConnection())
@@ -48,7 +49,7 @@ namespace EsbcProducerTest.Infra.RabbitMq.Providers
             using var channelProvider = new ChannelProvider(_connectionProvider.Object);
 
             // When
-            using var channel = channelProvider.GetChannel();
+            var channel = channelProvider.GetChannel();
 
             // Then
             channel.Should().NotBeNull();
@@ -60,7 +61,7 @@ namespace EsbcProducerTest.Infra.RabbitMq.Providers
             // Given
             _connection
                 .Setup(c => c.CreateModel())
-                .Returns(_channel)
+                .Returns(_channel.Object)
                 .Verifiable();
             _connectionProvider
                 .Setup(cp => cp.GetConnection())
@@ -76,6 +77,38 @@ namespace EsbcProducerTest.Infra.RabbitMq.Providers
             _connection.Verify(c => c.CreateModel(), Times.Once());
             _connectionProvider.Verify(cp => cp.GetConnection(), Times.Once());
             channel.Should().Be(chanell2);
+        }
+
+        [Fact]
+        public void ShouldConfigureAChanellOnDeclareQueue()
+        {
+            // Given
+            const string QueueName = "queueName";
+            const bool Durable = true;
+            const bool Exclusive = true;
+            const bool AutoDelete = true;
+            const IDictionary<string, object> Arguments = default;
+            var queueDeclareOk = new QueueDeclareOk(QueueName, 0, 0);
+            _channel
+                .Setup(c => c.QueueDeclare(QueueName, Durable, !Exclusive, !AutoDelete, Arguments))
+                .Returns(queueDeclareOk)
+                .Verifiable();
+            _connection
+                .Setup(c => c.CreateModel())
+                .Returns(_channel.Object)
+                .Verifiable();
+            _connectionProvider
+                .Setup(cp => cp.GetConnection())
+                .Returns(_connection.Object)
+                .Verifiable();
+            using var channelProvider = new ChannelProvider(_connectionProvider.Object);
+
+            // When
+            var expectedReturn = channelProvider.QueueDeclare(QueueName);
+
+            // Then
+            expectedReturn.Should().Be(channelProvider);
+            _connectionProvider.Verify(cp => cp.GetConnection(), Times.Once());
         }
     }
 }
